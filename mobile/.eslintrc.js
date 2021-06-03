35470e85f7948e92da02179316e88c2f068ab2cf
GrafSoul <grafsoul@gmail.com>
2021-06-03 12:47:07 +0500

    feat: Updated files - convert to Tabs

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,32 +1,15 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true,
-        node: true,
-    },
-    extends: ['plugin:react/recommended', 'eslint:recommended'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-        ecmaVersion: 11,
-        sourceType: 'module',
-        allowImportExportEverywhere: true,
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    plugins: ['react'],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
     rules: {
-        'react/prop-types': 0,
-        indent: ['error', 4],
-        quotes: ['error', 'single'],
-        semi: 0,
-        'no-unused-vars': [
-            'error',
-            { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
-        ],
+        'no-console': 1, // Means warning
+        'prettier/prettier': 2, // Means error
     },
 };
--- mobile/components/ProjectItem/index.tsx
+++ mobile/components/ProjectItem/index.tsx
@@ -1,39 +1,36 @@
 import React from 'react';
 import { Text, View, Pressable } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { styles } from './styles'
+import { styles } from './styles';
 
 interface ProjectItemProps {
     project: {
-        id: string,
-        title: string,
-        createdAt: string
-    }
+        id: string;
+        title: string;
+        createdAt: string;
+    };
 }
 
-const ProjectItem = ({project}: ProjectItemProps) => {
-
-    const {title, createdAt} = project
+const ProjectItem = ({ project }: ProjectItemProps) => {
+    const { title, createdAt } = project;
 
     const onPress = () => {
-        console.warn(`Open ${title}`)
-    }
+        console.warn(`Open ${title}`);
+    };
 
     return (
         <Pressable onPress={onPress} style={styles.root}>
             <View style={styles.iconContainer}>
-                <MaterialCommunityIcons 
-                    name='file-outline' 
-                    size={24} 
-                    color="gray" />
+                <MaterialCommunityIcons
+                    name="file-outline"
+                    size={24}
+                    color="gray"
+                />
             </View>
             <Text style={styles.title}>{title}</Text>
             <Text style={styles.time}>{createdAt}</Text>
         </Pressable>
-    )
-
-}
-
-
+    );
+};
 
 export default ProjectItem;
